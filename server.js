1cbd7ebdcda9ff524c45906484a4cfeb3f1eda48
Melodie Chi <chzwzrd@chzwz-2.local>
2018-02-04 16:05:38 -0800

    make .env, install dotenv, require dotenv

--- server.js
+++ server.js
@@ -1,3 +1,5 @@
+require('dotenv').config();
+
 // MODULES/VARIABLES
 // =====================================================================================
 var express = require('express')
@@ -10,22 +12,24 @@ var express = require('express')
 // require model for syncing
 var models = require('./models');
 
+var isDev = process.env.NODE_ENV === 'development';
+
 // MIDDLEWARE
 // =====================================================================================
 var app = express();
 app.set('port', process.env.PORT || 3000);
+app.set('view engine', 'handlebars');
+app.engine('handlebars', exphbs({ defaultLayout: 'main' }));
 app.use(express.static('public'));
 app.use(bodyParser.urlencoded({ extended: false }));
 // override the POST form action with ?_method=PUT & ?_method=DELETE
 app.use(methodOverride('_method'));
-app.engine('handlebars', exphbs({ defaultLayout: 'main' }));
-app.set('view engine', 'handlebars');
 
 app.use('/', routes);
 
 // LISTENER
 // =====================================================================================
-models.sequelize.sync().then(() => {
+models.sequelize.sync({ force: isDev }).then(() => {
     models.Burger.bulkCreate([
         {
             burger_name: "The Hulk Hogan",
